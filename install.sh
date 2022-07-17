4d5b64545c4b9b910233fddbab5076caeb5a204e
Donien <donien.96@hotmail.com>
2022-07-17 19:48:57 +0200

    Add logic to choose what parts to install (dconf / gnome)

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-# This is just a small install script for the gnome desktop on arch linux.
-# It installs a few packages and tweaks the desktop a little using `dconf`.
 
 install_packages () {
     sudo pacman -S --needed \
@@ -12,4 +10,45 @@ install_packages () {
 }
 
 
+apply_all_dconf () {
+    for conf_file in ./dconf/*.conf
+    do
+        # Catch empty / non existing dconf directory
+        [ -e "$conf_file" ] || continue
 
+        #dconf load / < "$conf_file"
+        echo "$conf_file"
+    done
+}
+
+
+
+
+
+# If no arguments are given just install packages
+if [ "$#" -eq 0 ]
+then
+    install_packages
+fi
+
+case "$1" in
+    --install)
+        install_packages
+        ;;
+
+    --tweak)
+        apply_all_dconf
+        ;;
+
+    --all)
+        install_packages
+        apply_all_dconf
+        ;;
+
+    *)
+        >&2 echo "Unrecognized argument!"
+        exit 3
+        ;;
+esac
+
+exit 0
